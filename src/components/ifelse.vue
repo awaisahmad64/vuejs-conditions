b56a6d19946cff5cb990d632716f00f55992ea0c
webxsolutions <awais6412l@gmail.com>
2021-06-08 16:44:39 +0500

    if else in vue js

---
+++ src/components/ifelse.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h2 v-if="show">This if statement rendering ture.</h2>
+    <h2 v-else >This else statement rendering false.</h2>
+    <button v-on:click="display">ToggleFuntion</button>
+</div>
+</template>
+<script>
+export default {
+   name: 'ifelse',data(){
+       return {show:true}
+   },methods:{
+       display(){
+           this.show=!this.show
+       }
+   }
+}
+</script>
